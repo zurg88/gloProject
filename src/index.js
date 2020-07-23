4eb49064c8859b229066429a53f1c3360a3a4964
Sergey Gnatenko <gnatenko@gmail.com>
2020-07-23 22:16:25 +0300

    update popup function for gift popup

--- src/index.js
+++ src/index.js
@@ -20,6 +20,10 @@ const freeVisit = document.querySelector('.free-visit');
 const freeVisitPopup = document.getElementById('free_visit_form');
 const callbackBtn = document.querySelector('.callback-btn');
 const callbackForm = document.getElementById('callback_form');
+const fixedGiftPopup = document.getElementById('gift');
+const fixedGift = document.querySelector('.fixed-gift');
 
 showPopup(freeVisit, freeVisitPopup);
 showPopup(callbackBtn, callbackForm);
+showPopup(fixedGift, fixedGiftPopup, true);
+
--- src/modules/popup.js
+++ src/modules/popup.js
@@ -1,12 +1,16 @@
-const showPopup = (elem, popup) => {
+const showPopup = (elem, popup, hiddenElement = false) => {
 	elem.addEventListener('click', () => {
 		popup.style.display = 'block';
+		if (hiddenElement === true) {
+			elem.style.opacity = "0";
+		}
 	});
 
 	popup.addEventListener('click', event => {
 		const target = event.target;
 		if (target.closest('.overlay') || target.closest('.close_icon') || target.closest('.close-btn')) {
 			popup.style.display = 'none';
+			elem.style.opacity = "1";
 		}
 	});
 };
