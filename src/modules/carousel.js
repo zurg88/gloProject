93d3a9d78b99ce7e734278a3d294d96fca7a6936
Sergey Gnatenko <gnatenko@gmail.com>
2020-07-29 01:08:45 +0300

    fix carousel

--- src/modules/carousel.js
+++ src/modules/carousel.js
@@ -1,41 +1,26 @@
 const carousel = () => {
-	const servicesSlider = document.querySelector('.services-slider'),
+	const slideContainer = document.querySelector('.slide-container'),
+		servicesSlider = document.querySelector('.services-slider'),
 		prev = servicesSlider.querySelector('.prev'),
 		next = servicesSlider.querySelector('.next');
 
-	let slideCollection = servicesSlider.querySelectorAll('.slide');
-
-	servicesSlider.style.width = Math.floor(slideCollection[0].offsetWidth * 6) + 'px';
-	let count = 0;
-
 	try {
 		prev.addEventListener('click', () => {
-			const cloneElem = slideCollection[count].cloneNode(true);
-			prev.before(cloneElem);
-			slideCollection[count].remove();
-			count++;
-			console.log(count);
-			if (count >= slideCollection.length - 1) {
-				count = 0;
-				slideCollection = servicesSlider.querySelectorAll('.slide');
-			}
+			const cloneElem = slideContainer.firstChild.cloneNode(true);
+			slideContainer.append(cloneElem);
+			slideContainer.firstChild.remove();
+			console.log(typeof(cloneElem));
+			console.log(cloneElem);
 		});
 	} catch (error) {
 		return;
 	}
 
-	let nextCount = 9;
-
 	next.addEventListener('click', () => {
-		const cloneElem = slideCollection [nextCount].cloneNode(true);
-		servicesSlider.prepend(cloneElem);
-		slideCollection[nextCount].remove();
-		nextCount--;
-		console.log(nextCount);
-		if (nextCount <= 0) {
-			nextCount = slideCollection.length - 1;
-			slideCollection = servicesSlider.querySelectorAll('.slide');
-		}
+		const cloneElem = slideContainer.lastChild.cloneNode(true);
+		slideContainer.prepend(cloneElem);
+		slideContainer.lastChild.remove();
+		console.log(cloneElem);
 	});
 };
 
