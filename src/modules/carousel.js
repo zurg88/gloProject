45fd1290c0abeaa4a587d72b8fd102d6ef736c45
Sergey Gnatenko <gnatenko@gmail.com>
2020-07-29 20:14:50 +0300

    fixed slider

--- src/modules/carousel.js
+++ src/modules/carousel.js
@@ -2,9 +2,26 @@ const carousel = () => {
 	const slideContainer = document.querySelector('.slide-container'),
 		servicesSlider = document.querySelector('.services-slider'),
 		prev = servicesSlider.querySelector('.prev'),
-		next = servicesSlider.querySelector('.next');
+		next = servicesSlider.querySelector('.next'),
+		slide = slideContainer.querySelector('.slide');
 
 	try {
+		let sliderWidth = 920;
+
+		if (document.documentElement.clientWidth < 768) {
+			sliderWidth = 600;
+		}
+		
+		if (document.documentElement.clientWidth < 992) {
+			sliderWidth = 760;
+		}
+
+		if (document.documentElement.clientWidth < 575) {
+			sliderWidth = 350;
+		}
+
+		servicesSlider.style.width = `${sliderWidth}px`;
+
 		prev.addEventListener('click', () => {
 			const cloneElem = slideContainer.firstElementChild.cloneNode(true);
 			slideContainer.append(cloneElem);
